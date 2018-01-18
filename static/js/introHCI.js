7705cd3ceeeb454e7d94e7c53832ff2e8b66f1d2
Ziliang Zhang <ziz137@ucsd.edu>
2018-01-18 10:36:40 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -15,4 +15,13 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
 }
